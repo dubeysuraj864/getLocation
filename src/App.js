8077f27daf8872ae7c0565ee872f6fbb00129dce
Suraj Dubey <dubeysuraj864@gmail.com>
2022-08-25 20:15:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import Weather from './components/Weather';
+
+
 
 function App() {
+
+  const[latitude, setLatitude] = useState([]);
+  const[longitude, setLongitude]= useState([]);
+  const[data, setData]= useState([])
+
+  useEffect(() => {
+
+    const fetchData = async () =>{
+    navigator.geolocation.getCurrentPosition(function(position){
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    });
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=dbbedde9ee53a9bcac9ac6dc3d629413`);
+
+    const data = await response.json();
+     setData(data);
+    console.log(data)
+
+    console.log("lati:", latitude)
+    console.log("longi:", longitude)
+  }
+   
+  fetchData();
+
+  },[latitude, longitude]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Weather App
+      <br />
+
+      <h1>{data.name}</h1>
+
+       {
+      (typeof data.main != 'undefined') ? (
+        <Weather prop={data} />
+      ) : (
+        <div></div>
+      )}
+     
     </div>
   );
 }
---
+++ src/components/Weather.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Weather(props) {
+  return <>Your location: {props.name}</>;
+}
+
+export default Weather;
